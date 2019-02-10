e0faefb28acdad523d4487be66a891e315cd104f
Artyom Katsap <artyom.katsap@gmail.com>
2019-02-11 00:03:05 +0200

    Added editing of UserRoles  (#2)
    * Initial commit
    
    * addded read me
    
    * added search
    
    * added filters
    
    * link to single userroles item
    
    * fxed editing user roles

--- roles_and_permissions/api/serializers.py
+++ roles_and_permissions/api/serializers.py
@@ -6,14 +6,24 @@ class PermissionSerializer(serializers.ModelSerializer):
         model = Permission
         fields = ("name",) 
 
-class RoleSerializer(serializers.ModelSerializer):
+class RoleSerializer(serializers.PrimaryKeyRelatedField, serializers.ModelSerializer):
     permissions = PermissionSerializer(many=True)
     class Meta:
         model = Role
         fields = ("name", 'permissions')
 
-class UserRolesSerializer(serializers.ModelSerializer):
-    role = RoleSerializer(many=True)
+class UserRolesSerializer(serializers.HyperlinkedModelSerializer):
+    role = RoleSerializer(many=True, queryset=Role.objects.all())
+    url = serializers.HyperlinkedIdentityField(view_name='view-user-roles', lookup_field='pk')
     class Meta:
         model = UserRole
-        fields = ('user_id', 'event_id', 'group_id', 'role', 'status', 'change_date')
+        fields = (
+                  'url',
+                  'user_id',
+                  'event_id',
+                  'group_id',
+                  'role',
+                  'status',
+                  'change_date',
+                  'user_name',
+                  'email')
--- roles_and_permissions/api/urls.py
+++ roles_and_permissions/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views.permissions import ListPermissionsView, PermissionsDetail
 from .views.roles import ListRolesView
-from .views.user_roles import ListUserRolesView
+from .views.user_roles import ListUserRolesView, UpdateUserRolesView
 
 urlpatterns = [
         path('permissions/', ListPermissionsView.as_view(),name='permissions-all'),
-        path('permissions/<int:pk>', PermissionsDetail.as_view(),name='permissions-all'),
-        path('roles/', ListRolesView.as_view(),name='permissions-all'),
-        path('user_roles/', ListUserRolesView.as_view(), name='user-roles')
+        path('permissions/<int:pk>', PermissionsDetail.as_view(),name='view-permissions'),
+        path('roles/', ListRolesView.as_view(),name='roles-all'),
+        path('user_roles/<int:pk>', UpdateUserRolesView.as_view(), name='view-user-roles'),
+        path('user_roles/', ListUserRolesView.as_view(), name='user-roles'),
 ]
